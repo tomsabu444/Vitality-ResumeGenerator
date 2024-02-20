173b458eb2ce6345358cab04b49ef10f7a09ece7
Jishnu Mohan <jishnumohan292@gmail.com>
2024-02-20 15:55:54 +0530

    commit

---
+++ Vitality-main/Vitality-main/README.md
@@ -0,0 +1,13 @@
+<div align=center>
+<h1> WebWizard - Website Competition </h1>
+</div>
+
+### Installion Through VS Code
+
+```cmd
+git clone https://github.com/tomsabu444/Vitality.git
+cd Vitality
+npm install
+npm run dev
+```
+Create Branch and then start Developing
---
+++ Vitality-main/Vitality-main/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+function Home() {
+  return (
+    <Container>
+      <h1>Resume Generator</h1>
+    </Container>
+  );
+}
+
+export default Home;
+
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+`;
