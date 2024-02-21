55ac0899f50260c9932304bd4b0661d2a6d32419
TOM SABU <tomsabu0000@gmail.com>
2024-02-21 20:56:51 +0530

    fgddg

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/Navbar";
-import Credits from "./pages/Credits";
+
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <NavBar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/credits" element={<Credits/>} />
+
         </Routes>
       </BrowserRouter>
     </>
