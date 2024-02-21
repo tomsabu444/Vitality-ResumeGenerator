95512a9c455dee9cfbc8fdf88cfb09f951a8ce48
Jishnu Mohan <jishnumohan292@gmail.com>
2024-02-21 19:00:51 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 function Home() {
-  return (
-    <Container>
-      <h1>Resume Generator</h1>
-    </Container>
-  );
+  return <Container></Container>;
 }
 
 export default Home;
 
-
 const Container = styled.div`
-display: flex;
-justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
