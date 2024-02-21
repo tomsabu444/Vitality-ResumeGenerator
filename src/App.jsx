bdfb6cd85a276dd29a3f3b05a3af9bf7841aee12
Justin James <justinjameselampurayidom@gmail.com>
2024-02-21 19:52:52 +0530

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
       <NavBar />
+      <Footer/>
         <Routes>
           <Route exact path="/" element={<Home />} />
         </Routes>
