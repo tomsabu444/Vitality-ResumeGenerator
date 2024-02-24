f7b9e3355f37712996bcda62b7876783ac4e948e
Jishnu Mohan <jishnumohan292@gmail.com>
2024-02-24 12:45:59 +0530

    creditpage-finalized

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -6,7 +6,6 @@ import Jishnu_image from "../images/jishnu.jpeg";
 import Tom_image from "../images/tomsabu.jpg";
 import Jot_image from "../images/justin.jpg";
 const Credits = () => {
-
   const teamMembers = [
     {
       name: "Jishnu Mohan",
@@ -14,7 +13,7 @@ const Credits = () => {
       linkedin: "https://www.linkedin.com/in/jishnu-mohan-b5837a227",
       email: "jishnumohan292@gmail.com",
       description: "Pro Designer",
-      image: Jishnu_image, 
+      image: Jishnu_image,
     },
     {
       name: "Tom Sabu",
@@ -22,7 +21,7 @@ const Credits = () => {
       linkedin: "https://www.linkedin.com/in/tomsabu444/",
       email: "tomsabu0000@gmail.com",
       description: "Developer",
-      image: Tom_image, 
+      image: Tom_image,
     },
     {
       name: "Justin James Elampurayidom",
@@ -30,43 +29,44 @@ const Credits = () => {
       linkedin: "https://www.linkedin.com/in/justin-james-elampurayidom",
       email: "justinjameselampurayidom@gmail.com",
       description: "Developer",
-      image: Jot_image, 
-    }, 
+      image: Jot_image,
+    },
   ];
 
   return (
     <Container>
-      <h2>Team Vitality</h2>
-      <p>Meet the amazing team behind this project</p>
-      <ul>
-        {teamMembers.map((member, index) => (
-          <Member key={index}>
-            <img src={member.image} alt={member.name} />
-            <div>
-              <strong>{member.name}</strong>    <br /> {member.description}
-              <br />
-
-              <a href={member.github} target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-github"></i>
-              </a>
-       
-
-              <a
-                href={member.linkedin}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-linkedin"></i>
-              </a>
-        
-   
-              <a href={`mailto:${member.email}`}>
-                <i className="far fa-envelope"></i>
-              </a>
-            </div>
-          </Member>
-        ))}
-      </ul>
+      <div className="con">
+        <h2>Team Vitality</h2>
+        <p>Meet the amazing team behind this project</p>
+        <ul>
+          {teamMembers.map((member, index) => (
+            <Member key={index}>
+              <img src={member.image} alt={member.name} />
+              <div>
+                <strong>{member.name}</strong> <br /> {member.description}
+                <br />
+                <a
+                  href={member.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-github"></i>
+                </a>
+                <a
+                  href={member.linkedin}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-linkedin"></i>
+                </a>
+                <a href={`mailto:${member.email}`}>
+                  <i className="far fa-envelope"></i>
+                </a>
+              </div>
+            </Member>
+          ))}
+        </ul>
+      </div>
     </Container>
   );
 };
@@ -76,9 +76,10 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  min-height: 100vh; /* Set minimum height to the full viewport height */
+  min-height: 93vh; /* Set minimum height to the full viewport height */
   padding: 20px;
   text-align: center;
+  background-color: #222222cd;
 
   h2 {
     color: #333;
@@ -86,6 +87,19 @@ const Container = styled.div`
 
   p {
     margin-bottom: 20px;
+  }
+  .con {
+    height: auto;
+    width: auto;
+    padding: 30px 30px;
+    border-radius: 15px;
+    background: linear-gradient(to top, grey, #ddd9d9);
+    box-shadow: 7px 7px 14px 8px #3b3a3ac5;
+  }
+
+  .con:hover img {
+    transform: scale(1.12);
+    border: 3px solid black;
   }
 `;
 
@@ -95,10 +109,12 @@ const Member = styled.li`
   margin-bottom: 20px;
 
   img {
-    width: 80px;
-    height: 80px;
+    width: 70px;
+    height: 70px;
     border-radius: 50%;
+    transition: all 0.3s;
     margin-right: 20px;
+    box-shadow: 7px 7px 14px #5e5b5bc5;
   }
 
   div {
