36ce4b3962d477513c07fe5c0dd251b0b2aae7fd
TOM SABU <tomsabu0000@gmail.com>
2024-02-24 12:21:42 +0530

    Landing-page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,195 +1,398 @@
 // React
-import React from 'react'
-import { Link } from 'react-router-dom'
-// CSS
-import './Header.scss'
-// components
-import StarsButton from '../startsButton/StartsButton'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import StarsButton from "../components/StartsButton";
 
 const Landing = () => {
-    return (
-        <div className="header">
-            {/* floating squares */}
-            <div className="square s1"></div>
-            <div className="square s2"></div>
-            <div className="square s3"></div>
-            <div className="square s4"></div>
-            <div className="square s5"></div>
-            {/* title */}
-            <div className="title-container">
-                <h1 className='title'>betteresume.</h1>
-            </div>
-            {/* animation */}
-            <div className="typewriter">
-                <div className="slide"><i></i></div>
-                <div className="paper"></div>
-                <div className="keyboard"></div>
-            </div>
-            {/* subtitle */}
-            <p className='description'>Start now and create your perfect ✨resume✨ to achieve your career goals!</p>
-            {/* custom button */}
-            <Link
-                to='/resume'
-                className='link'
-            >
-                <StarsButton title={'Start now'} />
-            </Link>
+  return (
+    <Container>
+      <div className="header">
+        {/* floating squares */}
+        <div className="square s1"></div>
+        <div className="square s2"></div>
+        <div className="square s3"></div>
+        <div className="square s4"></div>
+        <div className="square s5"></div>
+        {/* title */}
+        <div className="title-container">
+          <h1 className="title">Vitality Resume</h1>
         </div>
-    )
-}
+        {/* animation */}
+        <div className="typewriter">
+          <div className="slide">
+            <i></i>
+          </div>
+          <div className="paper"></div>
+          <div className="keyboard"></div>
+        </div>
+        {/* subtitle */}
+        <p className="description">
+          Start now and create your perfect ✨resume✨ to achieve your career
+          goals!
+        </p>
+        {/* custom button */}
+        <Link to="/resume" className="link">
+          <StarsButton title={"Start now"} />
+        </Link>
+      </div>
+    </Container>
+  );
+};
 
-export default Landing
+export default Landing;
 
 const Container = styled.div`
-.custom-btn2 {
-  position: relative;
-  padding: 12px 35px;
-  background: #92A0B5;
-  font-size: 17px;
-  font-weight: 500;
-  color: #263238;
-  border: 3px solid #92A0B5;
-  border-radius: 8px;
-  box-shadow: 0 0 0 #6c798e;
-  transition: all 0.3s ease-in-out;
-  cursor: pointer;
-  box-shadow: 0px 0px 15px -7px rgba(0, 0, 0, 0.75);
-  outline: none;
-}
-
-.star-1 {
-  position: absolute;
-  top: 20%;
-  left: 20%;
-  width: 25px;
-  height: auto;
-  filter: drop-shadow(0 0 0 #fffdef);
-  z-index: -5;
-  transition: all 1s cubic-bezier(0.05, 0.83, 0.43, 0.96);
-}
-
-.star-2 {
-  position: absolute;
-  top: 45%;
-  left: 45%;
-  width: 15px;
-  height: auto;
-  filter: drop-shadow(0 0 0 #fffdef);
-  z-index: -5;
-  transition: all 1s cubic-bezier(0, 0.4, 0, 1.01);
-}
-
-.star-3 {
-  position: absolute;
-  top: 40%;
-  left: 40%;
-  width: 5px;
-  height: auto;
-  filter: drop-shadow(0 0 0 #fffdef);
-  z-index: -5;
-  transition: all 1s cubic-bezier(0, 0.4, 0, 1.01);
-}
-
-.star-4 {
-  position: absolute;
-  top: 20%;
-  left: 40%;
-  width: 8px;
-  height: auto;
-  filter: drop-shadow(0 0 0 #fffdef);
-  z-index: -5;
-  transition: all 0.8s cubic-bezier(0, 0.4, 0, 1.01);
-}
-
-.star-5 {
-  position: absolute;
-  top: 25%;
-  left: 45%;
-  width: 15px;
-  height: auto;
-  filter: drop-shadow(0 0 0 #fffdef);
-  z-index: -5;
-  transition: all 0.6s cubic-bezier(0, 0.4, 0, 1.01);
-}
-
-.star-6 {
-  position: absolute;
-  top: 5%;
-  left: 50%;
-  width: 5px;
-  height: auto;
-  filter: drop-shadow(0 0 0 #fffdef);
-  z-index: -5;
-  transition: all 0.8s ease;
-}
-
-.custom-btn2:hover {
-  background: transparent;
-  color: #92A0B5;
-  box-shadow: 0 0 25px #5f6978;
-}
-
-.custom-btn2:hover .star-1 {
-  position: absolute;
-  top: -80%;
-  left: -30%;
-  width: 25px;
-  height: auto;
-  filter: drop-shadow(0 0 10px #fffdef);
-  z-index: 2;
-}
-
-.custom-btn2:hover .star-2 {
-  position: absolute;
-  top: -25%;
-  left: 10%;
-  width: 15px;
-  height: auto;
-  filter: drop-shadow(0 0 10px #fffdef);
-  z-index: 2;
-}
-
-.custom-btn2:hover .star-3 {
-  position: absolute;
-  top: 55%;
-  left: 25%;
-  width: 5px;
-  height: auto;
-  filter: drop-shadow(0 0 10px #fffdef);
-  z-index: 2;
-}
-
-.custom-btn2:hover .star-4 {
-  position: absolute;
-  top: 30%;
-  left: 80%;
-  width: 8px;
-  height: auto;
-  filter: drop-shadow(0 0 10px #fffdef);
-  z-index: 2;
-}
-
-.custom-btn2:hover .star-5 {
-  position: absolute;
-  top: 25%;
-  left: 115%;
-  width: 15px;
-  height: auto;
-  filter: drop-shadow(0 0 10px #fffdef);
-  z-index: 2;
-}
-
-.custom-btn2:hover .star-6 {
-  position: absolute;
-  top: 5%;
-  left: 60%;
-  width: 5px;
-  height: auto;
-  filter: drop-shadow(0 0 10px #fffdef);
-  z-index: 2;
-}
-
-.fil0 {
-  fill: #FFFDEF;
-}/*# sourceMappingURL=StarsButton.css.map */
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100vh;
+    padding: 0 2rem;
+    background: #222222cd;
+  }
+  .header .title-container {
+    color: #92a0b5;
+  }
+  .header .title-container .title {
+    font-size: 5rem;
+    width: 100%;
+    white-space: nowrap;
+    overflow: hidden;
+    animation: typing 2s;
+  }
+  .header .description {
+    z-index: 10;
+    word-wrap: break-word;
+    padding-top: 2rem;
+    font-size: 1.1rem;
+    text-align: center;
+    color: #fffbf7;
+  }
+  .header .link {
+    margin-top: 2rem;
+  }
+  .header .square {
+    background: rgba(255, 255, 255, 0.05);
+    border-radius: 20px;
+    position: absolute;
+    animation: float 5s linear infinite;
+    backdrop-filter: blur(5px);
+    z-index: 0;
+  }
+  .header .square.s1 {
+    left: 20vw;
+    top: 100px;
+    width: 60px;
+    height: 60px;
+  }
+  .header .square.s2 {
+    right: 12.5vw;
+    top: 450px;
+    width: 140px;
+    height: 140px;
+    animation-delay: 1s;
+    border-radius: 15px;
+  }
+  .header .square.s3 {
+    right: 25vw;
+    top: 120px;
+    width: 50px;
+    height: 50px;
+    animation-delay: 2s;
+    border-radius: 10px;
+  }
+  .header .square.s4 {
+    left: 5vw;
+    top: 320px;
+    width: 100px;
+    height: 100px;
+    animation-delay: 1.5s;
+  }
+  .header .square.s5 {
+    border-radius: 10px;
+    left: 14vw;
+    top: 70vh;
+    width: 80px;
+    height: 80px;
+    animation-delay: 2.5s;
+  }
+  @media screen and (max-width: 650px) {
+    .header .title-container .title {
+      font-size: 3.25rem;
+    }
+    .header .description {
+      font-size: 1rem;
+    }
+  }
+  @media screen and (max-width: 450px) {
+    .header {
+      padding: 0;
+    }
+    .header .title-container .title {
+      font-size: 2.35rem;
+    }
+    .header .description {
+      font-size: 0.9rem;
+    }
+  }
+  @keyframes typing {
+    from {
+      width: 0;
+    }
+  }
+  .typewriter {
+    --blue: #5c86ff;
+    --blue-dark: #275efe;
+    --key: #fff;
+    --paper: #eef0fd;
+    --text: #d3d4ec;
+    --tool: #fbc56c;
+    --duration: 3s;
+    position: relative;
+    -webkit-animation: bounce05 var(--duration) linear infinite;
+    animation: bounce05 var(--duration) linear infinite;
+  }
+  .typewriter .slide {
+    width: 92px;
+    height: 20px;
+    border-radius: 3px;
+    margin-left: 14px;
+    transform: translateX(14px);
+    background: linear-gradient(var(--blue), var(--blue-dark));
+    -webkit-animation: slide05 var(--duration) ease infinite;
+    animation: slide05 var(--duration) ease infinite;
+  }
+  .typewriter .slide:before,
+  .typewriter .slide:after,
+  .typewriter .slide i:before {
+    content: "";
+    position: absolute;
+    background: var(--tool);
+  }
+  .typewriter .slide:before {
+    width: 2px;
+    height: 8px;
+    top: 6px;
+    left: 100%;
+  }
+  .typewriter .slide:after {
+    left: 94px;
+    top: 3px;
+    height: 14px;
+    width: 6px;
+    border-radius: 3px;
+  }
+  .typewriter .slide i {
+    display: block;
+    position: absolute;
+    right: 100%;
+    width: 6px;
+    height: 4px;
+    top: 4px;
+    background: var(--tool);
+  }
+  .typewriter .slide i:before {
+    right: 100%;
+    top: -2px;
+    width: 4px;
+    border-radius: 2px;
+    height: 14px;
+  }
+  .typewriter .paper {
+    position: absolute;
+    left: 24px;
+    top: -26px;
+    width: 40px;
+    height: 46px;
+    border-radius: 5px;
+    background: var(--paper);
+    transform: translateY(46px);
+    -webkit-animation: paper05 var(--duration) linear infinite;
+    animation: paper05 var(--duration) linear infinite;
+  }
+  .typewriter .paper:before {
+    content: "";
+    position: absolute;
+    left: 6px;
+    right: 6px;
+    top: 7px;
+    border-radius: 2px;
+    height: 4px;
+    transform: scaleY(0.8);
+    background: var(--text);
+    box-shadow: 0 12px 0 var(--text), 0 24px 0 var(--text), 0 36px 0 var(--text);
+  }
+  .typewriter .keyboard {
+    width: 120px;
+    height: 56px;
+    margin-top: -10px;
+    z-index: 1;
+    position: relative;
+  }
+  .typewriter .keyboard:before,
+  .typewriter .keyboard:after {
+    content: "";
+    position: absolute;
+  }
+  .typewriter .keyboard:before {
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 7px;
+    background: linear-gradient(135deg, var(--blue), var(--blue-dark));
+    transform: perspective(10px) rotateX(2deg);
+    transform-origin: 50% 100%;
+  }
+  .typewriter .keyboard:after {
+    left: 2px;
+    top: 25px;
+    width: 11px;
+    height: 4px;
+    border-radius: 2px;
+    box-shadow: 15px 0 0 var(--key), 30px 0 0 var(--key), 45px 0 0 var(--key),
+      60px 0 0 var(--key), 75px 0 0 var(--key), 90px 0 0 var(--key),
+      22px 10px 0 var(--key), 37px 10px 0 var(--key), 52px 10px 0 var(--key),
+      60px 10px 0 var(--key), 68px 10px 0 var(--key), 83px 10px 0 var(--key);
+    -webkit-animation: keyboard05 var(--duration) linear infinite;
+    animation: keyboard05 var(--duration) linear infinite;
+  }
+  @keyframes bounce05 {
+    85%,
+    92%,
+    100% {
+      transform: translateY(0);
+    }
+    89% {
+      transform: translateY(-4px);
+    }
+    95% {
+      transform: translateY(2px);
+    }
+  }
+  @keyframes slide05 {
+    5% {
+      transform: translateX(14px);
+    }
+    15%,
+    30% {
+      transform: translateX(6px);
+    }
+    40%,
+    55% {
+      transform: translateX(0);
+    }
+    65%,
+    70% {
+      transform: translateX(-4px);
+    }
+    80%,
+    89% {
+      transform: translateX(-12px);
+    }
+    100% {
+      transform: translateX(14px);
+    }
+  }
+  @keyframes paper05 {
+    5% {
+      transform: translateY(46px);
+    }
+    20%,
+    30% {
+      transform: translateY(34px);
+    }
+    40%,
+    55% {
+      transform: translateY(22px);
+    }
+    65%,
+    70% {
+      transform: translateY(10px);
+    }
+    80%,
+    85% {
+      transform: translateY(0);
+    }
+    92%,
+    100% {
+      transform: translateY(46px);
+    }
+  }
+  @keyframes keyboard05 {
+    5%,
+    12%,
+    21%,
+    30%,
+    39%,
+    48%,
+    57%,
+    66%,
+    75%,
+    84% {
+      box-shadow: 15px 0 0 var(--key), 30px 0 0 var(--key), 45px 0 0 var(--key),
+        60px 0 0 var(--key), 75px 0 0 var(--key), 90px 0 0 var(--key),
+        22px 10px 0 var(--key), 37px 10px 0 var(--key), 52px 10px 0 var(--key),
+        60px 10px 0 var(--key), 68px 10px 0 var(--key), 83px 10px 0 var(--key);
+    }
+    9% {
+      box-shadow: 15px 2px 0 var(--key), 30px 0 0 var(--key),
+        45px 0 0 var(--key), 60px 0 0 var(--key), 75px 0 0 var(--key),
+        90px 0 0 var(--key), 22px 10px 0 var(--key), 37px 10px 0 var(--key),
+        52px 10px 0 var(--key), 60px 10px 0 var(--key), 68px 10px 0 var(--key),
+        83px 10px 0 var(--key);
+    }
+    18% {
+      box-shadow: 15px 0 0 var(--key), 30px 0 0 var(--key), 45px 0 0 var(--key),
+        60px 2px 0 var(--key), 75px 0 0 var(--key), 90px 0 0 var(--key),
+        22px 10px 0 var(--key), 37px 10px 0 var(--key), 52px 10px 0 var(--key),
+        60px 10px 0 var(--key), 68px 10px 0 var(--key), 83px 10px 0 var(--key);
+    }
+    27% {
+      box-shadow: 15px 0 0 var(--key), 30px 0 0 var(--key), 45px 0 0 var(--key),
+        60px 0 0 var(--key), 75px 0 0 var(--key), 90px 0 0 var(--key),
+        22px 12px 0 var(--key), 37px 10px 0 var(--key), 52px 10px 0 var(--key),
+        60px 10px 0 var(--key), 68px 10px 0 var(--key), 83px 10px 0 var(--key);
+    }
+    36% {
+      box-shadow: 15px 0 0 var(--key), 30px 0 0 var(--key), 45px 0 0 var(--key),
+        60px 0 0 var(--key), 75px 0 0 var(--key), 90px 0 0 var(--key),
+        22px 10px 0 var(--key), 37px 10px 0 var(--key), 52px 12px 0 var(--key),
+        60px 12px 0 var(--key), 68px 12px 0 var(--key), 83px 10px 0 var(--key);
+    }
+    45% {
+      box-shadow: 15px 0 0 var(--key), 30px 0 0 var(--key), 45px 0 0 var(--key),
+        60px 0 0 var(--key), 75px 0 0 var(--key), 90px 2px 0 var(--key),
+        22px 10px 0 var(--key), 37px 10px 0 var(--key), 52px 10px 0 var(--key),
+        60px 10px 0 var(--key), 68px 10px 0 var(--key), 83px 10px 0 var(--key);
+    }
+    54% {
+      box-shadow: 15px 0 0 var(--key), 30px 2px 0 var(--key),
+        45px 0 0 var(--key), 60px 0 0 var(--key), 75px 0 0 var(--key),
+        90px 0 0 var(--key), 22px 10px 0 var(--key), 37px 10px 0 var(--key),
+        52px 10px 0 var(--key), 60px 10px 0 var(--key), 68px 10px 0 var(--key),
+        83px 10px 0 var(--key);
+    }
+    63% {
+      box-shadow: 15px 0 0 var(--key), 30px 0 0 var(--key), 45px 0 0 var(--key),
+        60px 0 0 var(--key), 75px 0 0 var(--key), 90px 0 0 var(--key),
+        22px 10px 0 var(--key), 37px 10px 0 var(--key), 52px 10px 0 var(--key),
+        60px 10px 0 var(--key), 68px 10px 0 var(--key), 83px 12px 0 var(--key);
+    }
+    72% {
+      box-shadow: 15px 0 0 var(--key), 30px 0 0 var(--key),
+        45px 2px 0 var(--key), 60px 0 0 var(--key), 75px 0 0 var(--key),
+        90px 0 0 var(--key), 22px 10px 0 var(--key), 37px 10px 0 var(--key),
+        52px 10px 0 var(--key), 60px 10px 0 var(--key), 68px 10px 0 var(--key),
+        83px 10px 0 var(--key);
+    }
+    81% {
+      box-shadow: 15px 0 0 var(--key), 30px 0 0 var(--key), 45px 0 0 var(--key),
+        60px 0 0 var(--key), 75px 0 0 var(--key), 90px 0 0 var(--key),
+        22px 10px 0 var(--key), 37px 12px 0 var(--key), 52px 10px 0 var(--key),
+        60px 10px 0 var(--key), 68px 10px 0 var(--key), 83px 10px 0 var(--key);
+    }
+  }
 `;
